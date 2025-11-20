d17fe16000e4aed81928881d4b4d802f5433dffd
Main <azomatov_suhrab@mail.ru>
2025-11-20 17:33:47 +0300

    com1

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,16 +3,13 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import web.model.User;
 import web.service.UserService;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
 public class UserController {
 
     private final UserService userService;
@@ -21,6 +18,7 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     @GetMapping("/users")
     public String getAllUsers(Model model) {
         List<User> users = userService.getAllUsers();
@@ -29,13 +27,34 @@ public class UserController {
     }
 
     @GetMapping("user-create")
-    public String CreateUserFrom (User user) {
+    public String CreateUserForm (User user) {
         return "user-create";
     }
 
-    @PostMapping
+    @PostMapping("/user-create")
     public String CreateUser(User user) {
         userService.saveUser(user);
         return "redirect:/users";
     }
+
+    @PostMapping("/users/delete")
+    public String deleteUser(@RequestParam("id") Integer id) {
+        userService.deleteUser(id);
+        return "redirect:/users";
+    }
+
+    @GetMapping(value = "/user-update")
+    public String showEditForm(@RequestParam("id") Integer id, Model model) {
+        User user = userService.getUserById(id);
+        model.addAttribute("user", user);
+        return "user-update";
+    }
+
+
+    @PostMapping(value = "/user-update")
+    public String updateUser( User user) {
+        userService.updateUser(user);
+        return "redirect:/users";
+    }
+
 }
